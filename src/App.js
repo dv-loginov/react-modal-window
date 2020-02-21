a23ecd9abce3b7b393b80c4d561f28c98a1e8635
dilog <dilog@rambler.ru>
2020-02-21 19:39:05 +0300

    add svg pictures

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import useModal from "./components/Modal/useModal";
 import Picture from "./components/Picture/Picture";
 
 const App = () => {
-  const { open, openModal, closeModal } = useModal();
+  const { open, openModal, closeModal, id } = useModal();
 
   return (
     <div className="App">
       <h1>Modals in React!</h1>
-      <button onClick={openModal}>Open</button>
+      <button id='question' onClick={openModal}>Question</button>
+      <button id='success' onClick={openModal}>Success</button>
+      <button id='error' onClick={openModal}>Error</button>
+      <button id='info' onClick={openModal}>Info</button>
+      <button id='warning' onClick={openModal}>Warning</button> 
         {open ? (
           <Modal
             close={closeModal}
             render={() =>
               <>            
                <Picture 
-                  name={'test'}
-                  width={150}
-                  height={150}  
+                  name={id}
+                  width={125}
+                  height={125}  
                   />
               <h1>Сообщение</h1>
               <h3>еще текст</h3>
--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,13 +1,26 @@
 import React from "react";
-import TestPicture from "./Pictures/TestPicture"
+import Info from "./Pictures/InfoPicture";
+import Error from "./Pictures/ErrorPicture";
+import Success from "./Pictures/SuccessPicture";
+import Warning from "./Pictures/WarningPicture";
+import Question from "./Pictures/QuestionPicture";
 
 const Picture = (props) => {  
     
     switch(props.name) {
-            case "test":
-              return <TestPicture {...props} />;
-            default:
-              return <div />;
+        case "info":
+            return <Info {...props} />;
+        case "error":
+            return <Error {...props} />;
+        case "success":
+            return <Success {...props} />;
+        case "warning":
+            return <Warning {...props} />;
+        case "question":
+            return <Question {...props} />;
+                                
+        default:
+            return <div />;
     };
   };
   
